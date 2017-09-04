287d0249c67c6e7a2c9cadaec9beb90cd474b119
SanjayRamas <sansa091@gmail.com>
2017-09-04 13:02:21 +0530

    Update db_config.php

--- db_config.php
+++ db_config.php
@@ -1,14 +1,20 @@
 <?php
 
-$DB_host = "localhost";
-$DB_user = "root";
-$DB_pass = "";
-$DB_name = "partner";
+//$DB_host = "localhost";
+//$DB_user = "root";
+//$DB_pass = "";
+//$DB_name = "partner";
 
- try
+try
  {
-     $DBcon = new PDO("mysql:host={$DB_host};dbname={$DB_name}",$DB_user,$DB_pass);
-     $DBcon->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$DBcon = new PDO(
+    'postgresql:host='.getenv("POSTGRESQL_ADDON_HOST").';dbname='.getenv("POSTGRESQL_ADDON_DB"),
+    getenv("POSTGRESQL_ADDON_USER"),
+    getenv("POSTGRESQL_ADDON_PASSWORD")
+ 
+ 
+     //$DBcon = new PDO("mysql:host={$DB_host};dbname={$DB_name}",$DB_user,$DB_pass);
+    // $DBcon->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
  }
  catch(PDOException $e)
  {
